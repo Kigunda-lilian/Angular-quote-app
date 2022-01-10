dcce7415c5c90811c30ff2b5bbcdcfb98b646d10
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-10 16:19:58 +0300

    Auto-generated commit [ci skip]

--- src/app/highest-upvoted-quote-highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighestUpvotedQuoteHighlightDirective } from './highest-upvoted-quote-highlight.directive';
-
-describe('HighestUpvotedQuoteHighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighestUpvotedQuoteHighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/quote.ts
+++
@@ -1,5 +0,0 @@
-export class Quote {
-  constructor(public index:number, public user:string, public author:string, public quote:string,
-              public upVotes:number, public downVotes:number, public createdAt:Date) {
-  }
-}
